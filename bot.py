5ac008ade645d95d0a39b6a403c0b58c10be6ed7
Bryan Gibson <bvgibson92@gmail.com>
2020-02-15 18:28:10 -0600

    More work on bot

--- bot.py
+++ bot.py
@@ -1,12 +1,52 @@
 # Discord Bot 
 
 import os
-import discord
+#import discord
+import random
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.command(name='Hello', help='Responds with a random meme from Marnie & Friends')
+async def greeting(ctx):
+    meme_quotes = [
+        'Wanna play some PoE?!',
+        'MICHI!',
+        'Do you... Need something?'
+    ]
+
+    response = random.choice(meme_quotes)
+    await ctx.send(response)
+
+@bot.command(name='roll', help='Simulates dice rolls, idiot.')
+async def roll(ctx, dice_load: str):
+    num_dice = dice_load[dice_load.index('d')]
+    num_sides = dice_load['d':]
+    dice = [
+        str(random.choice(range(1, num_sides + 1)))
+        for x in range(num_dice)
+    ]
+    await ctx.send(', '.join(dice))
+
+bot.run(TOKEN)
+
+
+
+"""
+class CustomClient(discord.Client):
+    async def on_ready(self):
+        print(f'{self.user} has connected to Discord!')
+
+
 
 client = discord.Client()
 
@@ -23,4 +63,40 @@ async def on_ready():
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
-client.run(TOKE
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, I have no idea what Im doing!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    meme_quotes = [
+        'Wanna play some PoE?!',
+        'MICHI!',
+        'Do you... Need something?'
+    ]
+
+    if message.content == 'Hello':
+        response = random.choice(meme_quotes)
+        await message.channel.send(response)
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('error.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
+
+#client = CustomClient()
+client.run(TOKEN)
+
+"""
