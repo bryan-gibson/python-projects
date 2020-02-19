26796f46a1e080c3001fb6cb4c0ddf39b8195971
Bryan Gibson <bvgibson92@gmail.com>
2020-02-19 00:13:11 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ async def roll(ctx, dice_load: str):
     
     dice_num = int(dice_load[:dice_load.index('d')])
     dice_sides = int(dice_load[dice_load.index('d') + 1:dice_load.index('+' or '-')])
-    num_offset = str(dice_load[dice_load.index('+' or '-'):])
+    num_offset = str(dice_load[dice_load.index('+' or '-') + 1:])
     dice_sum = 0
     dice = [
         str(random.choice(range(1, dice_sides + 1)))
@@ -42,12 +42,16 @@ async def roll(ctx, dice_load: str):
         dice_sum += int(dice[_])
     dice_sum += int(num_offset)
     #await ctx.send(', '.join(dice))
-    await ctx.send("{} ([{}] {})".format(dice_sum, ', '.join(dice), num_offset))
+    await ctx.send("{} ([{}] + {})".format(dice_sum, ', '.join(dice), num_offset))
 
-@bot.command(name='Christionary', help='Shit Chris says but no one understands.')
-async def christionary(ctx, chris_word: str):
-    if (chris_word.casefold() == "jobber"):
+@bot.command(name='Christionary', help='Shit Chris says.')
+async def christionary(ctx, *, chris_word: str):
+    if chris_word.casefold() in ["jobber"]:
         await ctx.send("Someone who loses a lot? A wrestler?")
+    if chris_word.casefold() in ["chest beefers"]:
+        await ctx.send("Boobs. <:BaldChamp:663564301809876992>")
+    if chris_word.casefold() in ["I was doing a gnome"]:
+        await ctx.send("and I fell on its shaft.")
 
 bot.run(TOKEN)
 
